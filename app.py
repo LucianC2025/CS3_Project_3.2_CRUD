b1849c1faea6f4272313edb58b1ec87f0bb66352
LucianC2025 <lucian.cruz2025@gbwl.org>
2025-03-03 13:55:13 +0000

    got IT SOME WHAT WORKING!L!K!!!!!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drinks.db'
-# Flash Messages 
+# Flash Messages (this is required)
 app.config['SECRET_KEY'] = 'supersecretkey'
 db = SQLAlchemy(app)
 
@@ -15,12 +15,14 @@ db = SQLAlchemy(app)
 class Drink(db.Model):
     # NOTE: PickleType: Holds Python objects, which are serialized using pickle.
     # NOTE: __init__ : A constructor. Used to define how an object is created before storing it in the database
+    # NOTE: Text(size) Holds a string with a maximum length of 65,535 bytes
 
     id = db.Column(db.Integer, primary_key=True)
+    # Store the ingredients as a serialized list 
     ingredients = db.Column(db.PickleType, nullable = False)
-    # Text(size) Holds a string with a maximum length of 65,535 bytes
-    name = db.Column(db.Text, nullable=False) # JSON stored as text
+    name = db.Column(db.Text, nullable=False) 
 
+    # Goal: esure that the ingredients are a list.. I want the code on the HTML file to appear as a bulleted list.. but I want the ingredients to be stored in the dataframe as a list.. 
     def __init__(self, name, ingredients):
         if not isinstance(ingredients, list):
             raise ValueError('Ingredients must be a list.')
@@ -30,11 +32,11 @@ class Drink(db.Model):
             print('Calm down there...')
 
         self.name = name
-        # NOTE: json.dumps() stores a list as a string
+        # NOTE: json.dumps() stores a serialized list as a JSON string
         self.ingredients = json.dumps(ingredients) 
     
     def get_ingredients(self):
-        # Turn the json string back into a list for python purposes
+        # Turn the JSON string back into a list for python purposes
         return json.loads(self.ingredients)
 
 # ***** End of Drink CLASS *****
@@ -42,14 +44,15 @@ class Drink(db.Model):
 
 
 
-
 # Flask Route for displaying all tasks
+
+# GOAl: When you add ingredients it should be a bullet list below the [] box and not create another box
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # ***** POST (add) method ******
     if request.method == 'POST':
         name = request.form.get('name')
-        ingredients = request.form.getlist('ingredients')
+        ingredients = request.form.getlist('ingredients[]')
 
         # NOTE: flash() is apart of Flask, and is used to send temporary messages to the user, like error notifications. The message is stored temporarly and dissapears shortly after being displayed
         # NOTE: flash('message', 'category') 
@@ -57,17 +60,17 @@ def index():
             #For example  url_for('index') looks for the Flask route function index not the HTML file index.html
 
         if not name or not ingredients: 
-            flash('Please enter a name and at least on ingredient', 'error')
+            flash('Please enter a name and at least one ingredient', 'error')
             return redirect(url_for('index'))
         
         if len(ingredients) >=50: 
-            flash('Too many ingredients! Max: 50.', 'error')
+            flash('Too many ingredients! Maximum allowed is 50.', 'error')
             return redirect(url_for('index'))
         
         new_drink = Drink(name, ingredients)
         db.session.add(new_drink)
         db.session.commit()
-        flash("You created a drink!", "success")
+        flash("Drink created sucessfully!", "success")
 
         return redirect(url_for('index'))
     # ***** end of POST (add) method ******
@@ -75,8 +78,6 @@ def index():
     drinks = Drink.query.all()
     return render_template('index.html', drinks = drinks)
  #***** end of INDEX function ******
-
-
      
 
 
